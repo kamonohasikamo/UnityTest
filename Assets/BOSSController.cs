49309ac558fe1a2cddd9dd945d908f332a3e3ba6
cs16048 <arrowsuke@yahoo.co.jp>
2018-11-25 18:49:59 +0900

    BOSS(仮)とタイマー機能の実装

---
+++ Assets/BOSSController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOSSController : MonoBehaviour {
+    float fallSpeed = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+        if (UIController.score == 100)
+        {
+            fallSpeed = 0.5f;
+        }
+        transform.Translate(0, -fallSpeed, 0, Space.World);
+
+        if(transform.position.y < 810)
+        {
+            fallSpeed = 0;
+        }
+
+        if (UIController.score == 550)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
